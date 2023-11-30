069eee6fc09515a5fcb5d4602e14257205856502
sylvio.ros@msn.com <sylvio.ros@msn.com>
2023-11-30 19:37:21 +0000

    Stages criadas com sucesso. Próximo passo: adicionar testes nas fontes.

--- models/staging/sap_adw/stg_adw__person.sql
+++ models/staging/sap_adw/stg_adw__person.sql
@@ -4,7 +4,7 @@ with
             cast(businessentityid as int) as idbusiness
             ,cast(firstname || ' ' || lastname as string) as nome_funcionario
             ,cast(persontype as string) as tipo_funcionario
-            ,case persontype
+            ,cast(case persontype
                 when 'EM' then 'Employee (non-sales)'
                 when 'GC' then 'General contact'
                 when 'IN' then 'Individual customer'
@@ -12,8 +12,11 @@ with
                 when 'SP' then 'Sales person'
                 when 'VC' then 'Vendor contact'
                 else 'Others'
-            end as tipo_funcionario_descr
+            end as string) as tipo_funcionario_descr
         from {{ source('adw', 'person') }}
     )
-select *
-from src_person
+--select *
+--from src_person
+
+select tipo_funcionario_descr from src_person 
+limit 5
